7714fff262d517adf29da95ad2adea64a89a4451
SIonut0122 <sionut0122@yahoo.com>
2020-04-08 14:20:28 +0100

    Updates

---
+++ precache-manifest.decb109ebb6de749edfdd050466078fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72748a58e5b9ed9c663efa9a29345d0",
+    "url": "/LiveChatV2.0/index.html"
+  },
+  {
+    "revision": "0cba18058ab55a1b1d8e",
+    "url": "/LiveChatV2.0/static/css/main.4481fdd0.chunk.css"
+  },
+  {
+    "revision": "babd2d25a41d6947c56d",
+    "url": "/LiveChatV2.0/static/js/2.747a9d08.chunk.js"
+  },
+  {
+    "revision": "b1ac8b734229fa843eb97e2dc0e50e60",
+    "url": "/LiveChatV2.0/static/js/2.747a9d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cba18058ab55a1b1d8e",
+    "url": "/LiveChatV2.0/static/js/main.bf51c002.chunk.js"
+  },
+  {
+    "revision": "1c32f0c9830402ddc2e0",
+    "url": "/LiveChatV2.0/static/js/runtime-main.8a6e56bb.js"
+  },
+  {
+    "revision": "e45ebabe3d504412742f1e2b0179fc7c",
+    "url": "/LiveChatV2.0/static/media/chat_logo.e45ebabe.png"
+  },
+  {
+    "revision": "48ba30982acdf46ccc9fdec4c90e6988",
+    "url": "/LiveChatV2.0/static/media/doi.48ba3098.png"
+  },
+  {
+    "revision": "a431eaad140cb7ef5f987e6d5a22ab6f",
+    "url": "/LiveChatV2.0/static/media/message.a431eaad.mp3"
+  },
+  {
+    "revision": "6d2fb720b13fe886eaf6e5a1cce3690d",
+    "url": "/LiveChatV2.0/static/media/unu.6d2fb720.png"
+  }
+]);
